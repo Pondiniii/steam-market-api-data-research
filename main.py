bcbfc54671ee6060f51dcc4f29a1c0126dd9e8be
Pondiniii <jakubpondo@protonmail.com>
2024-10-20 16:53:30 +0200

    cleaning code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
 import requests
 import psycopg2
-from secrets import db_secrets, api_secrets, bot_token, chat_id
+from secrets import db_secrets, bot_token, chat_id, webshare_proxy_api_key
 from database import insert_listing_into_db
 from request_tools import gen_market_link, response_parser
 import time
@@ -9,36 +8,23 @@ import urllib.parse
 import asyncio
 from telegram import Bot
 import traceback
-from proxy_tools import get_proxies
 import threading
 import random
 
-def update_proxies():
-    current_proxies = get_proxies()
-    global current_proxies 
-    while True:
-        current_proxies = get_proxies() 
-        print("Lista proxy odnowiona.")
-        time.sleep(30 * 60)  # Aktualizacja co 30 minut
 
 def main():
-    # Define the list of qualities to process
-    qualities = ['Battle-Scarred', 'Factory New', 'Minimal Wear', 'Field-Tested', 'Well-Worn']
-    
-    # Initialize error backoff timer
-    error_backoff = 30 * 60  # Start with 30 minutes in seconds
-
-    # Define the item name (since you're only scraping Desert Eagle | Heat Treated)
-    item_name = 'Desert Eagle | Heat Treated'
+    # Error timerStart with 30 min jail time
+    error_backoff = 30 * 60
+    paint_seed = None
+    response = None
 
     while True:
         try:
             connection = psycopg2.connect(**db_secrets)
-            for quality in qualities:
+            for quality in ['Factory New', 'Minimal Wear', 'Field-Tested', 'Well-Worn', 'Battle-Scarred']:
                 start = 0
                 count = 100
                 url = gen_market_link(start, count, quality)
-                
 
                 for _ in range(5):
                     proxy = random.choice(current_proxies)
@@ -58,25 +44,16 @@ def main():
                 if response.status_code != 200:
                     raise ValueError(f"Failed to fetch data from {url} after 5 retries")
 
-        
                 total_count = response.json().get('total_count', 0)
                 if total_count == 0:
-                    print(f"No listings found for quality '{quality}'.")
-                    continue  # Skip to the next quality
-                
-                # Calculate max_pages based on total_count and count
-                max_pages = (total_count + count - 1) // count  # Round up
-                
+                    raise ValueError(f"No listings found for quality '{quality}'.")
+
+                max_pages = (total_count + count - 1) // count
                 print(f"Quality: {quality}. Total listings: {total_count}. Total pages to process: {max_pages}")
-                
-                # Now we loop through all the pages
+
                 for page in range(max_pages):
-                    # Update the URL for the current page
                     url = gen_market_link(start, count, quality)
-                    
-                    # Rate limit to avoid spamming Steam
                     steam_rate_limit()
-                    
                     for _ in range(5):
                         proxy = random.choice(current_proxies)
                         try:
@@ -91,65 +68,44 @@ def main():
                         except Exception as e:
                             print(f"Other error: {e}")
                         time.sleep(60)
-
                     if response.status_code != 200:
                         raise ValueError(f"Failed to fetch data from {url} after 5 retries")
 
-
-                    # Parse the response and process listings
                     listings = response_parser(response)
-                    
                     if not listings:
                         print("No more listings found.")
                         break  # Exit loop if no more listings are found
-                   
 
                     should_skip_remaining_pages = False
-
-                    # Process each listing
                     for listing in listings:
-                        # Format the price
+                        listing_id = listing['listing_id']
                         price_cents = int(listing['price'])
-                        price_dollars = price_cents / 100  # Convert to dollars
-
+                        price_dollars = price_cents / 100
                         if price_dollars > 100:
                             print(f"Price {price_dollars} exceeds $100, skipping remaining listings and pages for quality {quality}")
                             should_skip_remaining_pages = True
-                            break  # Przerwij pętlę przetwarzającą oferty
+                            break
 
-                        listing_id = listing['listing_id']
-                        
                         if not should_process_listing(listing_id, connection):
-                            print(f"Skipping listing {listing_id}, already processed.")
                             continue
-                        
-                        # Insert the listing into the database
-                        insert_listing_into_db(listing, connection)
-                        
-                        for _ in range(5):  # Wykonaj maksymalnie 5 prób
+
+                        for _ in range(5):
                             paint_seed = fetch_paint_seed(listing['inspection_link'])
                             if paint_seed is not None:
-                                break  # Jeśli paint_seed jest prawidłowy, przerywamy pętlę
-                            time.sleep(60)  # Czekamy 60 sekund przed kolejną próbą
-
+                                break
+                            time.sleep(60)
                         if paint_seed is None:
                             raise ValueError("Paint Seed is None after 5 retries")
 
-                        print(f"Paint seed for listing {listing_id} (Quality: {quality}): {paint_seed}")
-
-                        # Update the database with the fetched paint_seed
+                        insert_listing_into_db(listing, connection)
                         cursor = connection.cursor()
                         cursor.execute("UPDATE listings SET paint_seed = %s WHERE listing_id = %s", (paint_seed, listing_id))
                         connection.commit()
                         cursor.close()
-                        
-                        # Send notification if the paint_seed meets criteria
-                        if paint_seed is not None and meets_criteria(paint_seed):
-                            # Cena jest na pewno ≤ 100$, więc nie musimy tego sprawdzać
-                            market_link = construct_market_link(item_name, quality)
-                            rank2 = [109, 116, 134, 158, 168, 225, 338, 354, 356, 365, 370, 386, 406, 426, 433, 441, 483, 537, 542, 592, 607, 611, 651, 668, 673, 696, 730, 743, 820, 846, 856, 857, 870, 876, 878, 882, 898, 900, 925, 942, 946, 951, 953, 970, 998]
 
-                            # Sprawdzenie, do którego rankingu należy paint_seed
+                        if paint_seed is not None and meets_criteria(paint_seed):
+                            market_link = construct_market_link('Desert Eagle | Heat Treated', quality)
+                            rank2 = [109, 116, 134, 158, 168, 225, 338, 354, 356, 365, 370, 386, 406, 426, 433, 441, 483, 537, 542, 592, 607, 611, 651, 668, 673, 696, 730, 743, 820, 846, 856, 857, 870, 876, 878, 882, 898, 900, 925, 942, 946, 951, 953, 970, 998]
                             rank_name = "rank2" if paint_seed in rank2 else "rank1"
 
                             message = (
@@ -159,24 +115,21 @@ def main():
                                 f"Inspect link: {listing['inspection_link']}"
                             )
 
-                            print(message)
                             send_telegram_message(message)
-                        
-                        # Rate limit for paint_seed API
+
                         time.sleep(fetch_paint_seed_rate_limit())
 
-                    # Po pętli przetwarzającej oferty
+                    # if price < 100$ then skip checking rest to save requests bandwidth
                     if should_skip_remaining_pages:
-                        break  # Przerwij pętlę stron, przejdź do następnej jakości
+                        break
 
                     # Move to the next page
                     start += count
             
             connection.close()
-            
             error_backoff = 30 * 60
+
         except Exception as e:
-            # Print the error for debugging
             print(f"An error occurred: {e}")
             send_telegram_message(str(e))
             traceback.print_exc()
@@ -188,9 +141,10 @@ def main():
             # Increase the backoff timer by 30 minutes for the next potential error
             error_backoff += 30 * 60
 
+
 steam_last_request_time = 0
 def steam_rate_limit():
-    # rate limit teraz to 5 sekund przed kolejnym zapytaniem
+    # rate limit in seconds before next request - new request every 5 sec
     global steam_last_request_time
     current_time = time.time()
     elapsed_time = current_time - steam_last_request_time
@@ -200,6 +154,7 @@ def steam_rate_limit():
         time.sleep(sleep_time)
     steam_last_request_time = time.time()
 
+
 fetch_paint_seed_last_request_time = 0
 def fetch_paint_seed_rate_limit():
     global fetch_paint_seed_last_request_time
@@ -208,23 +163,19 @@ def fetch_paint_seed_rate_limit():
     min_interval = 60 / 30  # 30 requests per minute
     if elapsed_time < min_interval:
         sleep_time = min_interval - elapsed_time
-        print(f"Waiting {sleep_time:.2f} seconds before the next csinventory API request.")
+        print(f"Waiting {sleep_time:.2f} seconds before the next local API request.")
         fetch_paint_seed_last_request_time = current_time + sleep_time
         return sleep_time
     else:
         fetch_paint_seed_last_request_time = current_time
         return 0
 
+
 def fetch_paint_seed(inspection_link):
     try:
-        # Encode the inspect link for use in your API
         encoded_link = urllib.parse.quote(inspection_link, safe='')
-        # Your local API address
         api_url = f"http://localhost:80/?url={encoded_link}"
-        
-        print(f"Sending request to your API: {api_url}")
         response = requests.get(api_url)
-        
         if response.status_code != 200:
             print(f"Error fetching paint_seed for {inspection_link}")
             return None
@@ -236,15 +187,17 @@ def fetch_paint_seed(inspection_link):
         else:
             print(f"No paint_seed found for {inspection_link}")
             return None
+
     except Exception as e:
         print(f"Error in fetch_paint_seed: {e}")
         return None
+
 
 def meets_criteria(paint_seed):
     numbers = [490, 148, 69, 704, 16, 48, 66, 67, 96, 111, 117, 159, 259, 263, 273, 297, 308, 321, 324, 341, 347, 461, 482, 517, 530, 567, 587, 674, 695, 723, 764, 772, 781, 790, 792, 843, 880, 885, 904, 948, 990, 109, 116, 134, 158, 168, 225, 338, 354, 356, 365, 370, 386, 406, 426, 433, 441, 483, 537, 542, 592, 607, 611, 651, 668, 673, 696, 730, 743, 820, 846, 856, 857, 870, 876, 878, 882, 898, 900, 925, 942, 946, 951, 953, 970, 998]
     return paint_seed in numbers
 
-# Implement the function to send a notification via Telegram
+
 async def send_telegram_message_async(message):
     try:
         bot = Bot(token=bot_token)
@@ -257,8 +210,10 @@ async def send_telegram_message_async(message):
     except Exception as e:
         print(f"Error sending message: {e}")
 
+
 def send_telegram_message(message):
     asyncio.run(send_telegram_message_async(message))
+
 
 def should_process_listing(listing_id, connection):
     cursor = connection.cursor()
@@ -278,16 +233,43 @@ def should_process_listing(listing_id, connection):
         # Listing does not exist, need to process
         return True
 
+
 def construct_market_link(item_name, quality):
-    # Construct the Steam market link for the specific item and quality
     base_url = "https://steamcommunity.com/market/listings/730/"
-    # Combine item name and quality
     item_name_with_quality = f"{item_name} ({quality})"
-    # URL-encode the item name with quality
     item_name_encoded = urllib.parse.quote(item_name_with_quality, safe='')
     full_url = f"{base_url}{item_name_encoded}"
     return full_url
 
+
+def get_proxies():
+    headers = {
+        "Authorization": f"Token {webshare_proxy_api_key}"
+    }
+    response = requests.get("https://proxy.webshare.io/api/proxy/list/", headers=headers)
+
+    if response.status_code == 200:
+        proxy_data = response.json()
+        proxies_list = []
+        for proxy in proxy_data['results']:
+            proxy_format = {
+                'https': f"socks5://{proxy['proxy_address']}:{proxy['ports']['socks5']}"
+            }
+            proxies_list.append(proxy_format)
+        return proxies_list
+    else:
+        print(f"Error: {response.status_code}")
+        return []
+
+
+current_proxies = get_proxies()
+def update_proxies():
+    global current_proxies
+    current_proxies = get_proxies()
+    while True:
+        current_proxies = get_proxies()
+        print("Lista proxy odnowiona.")
+        time.sleep(30 * 60)  # Aktualizacja co 30 minut
 
 
 if __name__ == "__main__":
--- proxy_tools.py
+++ proxy_tools.py
@@ -1,29 +1,5 @@
 import requests
-from secrets import webshare_proxy_api_key
 
-def get_proxies():
-    url = "https://proxy.webshare.io/api/proxy/list/"
-    headers = {
-        "Authorization": f"Token {webshare_proxy_api_key}"
-    }
-    
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
-        proxy_data = response.json()
-        proxies_list = []
-        
-        # Parsowanie odpowiedzi i przekształcanie do formatu SOCKS5
-        for proxy in proxy_data['results']:
-            proxy_format = {
-                'socks5': f"socks5://{proxy['proxy_address']}:{proxy['ports']['socks5']}"
-            }
-            proxies_list.append(proxy_format)
-        
-        return proxies_list
-    else:
-        print(f"Błąd: {response.status_code}")
-        return []
 
 # Testowanie funkcji
 proxies = get_proxies()
