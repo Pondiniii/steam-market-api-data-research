bf778c2d9d5a410da54b10ee5d64b70cfef53f99
Pondiniii <jakubpondo@protonmail.com>
2024-10-30 12:43:20 +0100

    bug fix - autobuy bad variable price_pln -> price_cents

--- main.py
+++ main.py
@@ -135,9 +135,9 @@ def process_quality(quality):
                             if should_autobuy(paint_seed, quality, price_pln):
                                 try:
                                     send_telegram_message(f"Auto buying: Desert Eagle | Heat Treated ({quality}) \n"
-                                                          f"price + fee: {price_pln + fee} fee: {fee}")
+                                                          f"price + fee: {price_cents+ fee} fee: {fee}")
                                     steam_client.market.buy_item(f'Desert Eagle | Heat Treated ({quality})',
-                                                                            listing_id, price_pln + fee, fee, GameOptions.CS,
+                                                                            listing_id, price_cents + fee, fee, GameOptions.CS,
                                                                             Currency.PLN)
                                     send_telegram_message(f"Autobuy successful")
                                 except Exception as e:
